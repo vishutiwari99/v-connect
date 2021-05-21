49dacd3a9a007fd1e119fff931cc248b689623ee
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-05-21 12:46:30 +0530

    Auth feature is complete

--- controller/authController.js
+++ controller/authController.js
@@ -96,6 +96,7 @@ const authController = {
 
             jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, async (err, result) => {
                 if (err) return res.status(400).json({ msg: "Please login now." })
+                console.log(result);
 
                 const user = await Users.findById(result.id).select("-password")
                     .populate('followers following', 'avatar username fullname followers following')
