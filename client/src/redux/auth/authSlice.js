579b466a698f1aafa05ca462ab3366c725aa554a
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-05-22 17:21:52 +0530

    auth reducer is fixed

--- client/src/redux/auth/authSlice.js
+++ client/src/redux/auth/authSlice.js
@@ -1,19 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { postDataAPI } from '../../utils/fetchData'
 
+export const loginAPI = createAsyncThunk(
+    'auth/loginAPI',
+    async (data) => {
+        const response = await postDataAPI('login', data)
+        return response;
+    }
+)
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        value: 0,
+
     },
-    reducers: {
-        auth: (data) => {
-            console.log(data);
+    extraReducers: {
+        [loginAPI.pending]: (state, action) => {
+            state.status = 'loading'
         },
+        [loginAPI.fulfilled]: (state, { payload }) => {
+            console.log(payload);
+            state.token = payload.data.access_token
+            state.user = payload.data.user
+        },
+        [loginAPI.rejected]: (state, action) => {
+            state.status = 'Failed'
+        },
+
 
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { auth } = authSlice.actions
+// export const { loginAPI } = authSlice.actions
 
 export default authSlice.reducer
