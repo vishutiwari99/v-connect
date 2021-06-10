75e8683b3390d447ba35425775c7bb0755e1d470
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-10 16:44:42 +0530

    Toast added and Fixed some bugs

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify';
-
+import { injectStyle } from "react-toastify/dist/inject-style";
 import './App.css';
-import Alert from './components/alert/Alert';
+import Notify from './components/notify/Notify';
+import { ToastContainer } from 'react-toastify'
+
 import PageRender from './customRouter/PageRender';
 import Home from './pages/home';
-import Login from './pages/login';
-import Register from './pages/register';
 
+if (typeof window !== "undefined") {
+  injectStyle();
+}
 
 function App() {
+
   return (
     <Router>
+      <ToastContainer />
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="max-w-2xl mx-auto">
-          {/* <Notify /> */}
-          {/* <ToastContainer /> */}
-          {/* <Alert /> */}
+          <Notify />
           <Route exact path="/" component={Home} />
           <Route exact path="/:page" component={PageRender} />
           <Route exact path="/:page/:id" component={PageRender} />
--- client/src/redux/actions/authAction.js
+++ client/src/redux/actions/authAction.js
@@ -10,17 +10,11 @@ export const login = (data) => async (dispatch) => {
         dispatch({ type: 'NOTIFY', payload: { loading: true } })
         const res = await postDataAPI('login', data);
         localStorage.setItem('firstLogin', true);
-        dispatch(
-            {
-                type: 'AUTH',
-                payload: {
-                    token: res.data.access_token,
-                    username: res.data.user
-                }
-            })
+        dispatch({ type: 'AUTH', payload: { token: res.data.access_token, username: res.data.user } })
+        dispatch({ type: 'NOTIFY', payload: { success: res.data.msg } })
         console.log(res)
     } catch (err) {
-        dispatch({ type: 'NOTIFY', payload: { error: err.response.data.message } })
+        dispatch({ type: 'NOTIFY', payload: { error: err.response.data.msg } })
 
     }
 }
