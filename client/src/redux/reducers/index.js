3bdd7c7c612e47e3d9a9c76cac64c59f6b37f09a
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-13 10:59:55 +0530

    added theme

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
+import 'material-icons/iconfont/filled.css';
 import auth from './authReducer'
 import alert from './alertReducer'
+import theme from './themeReducer'
 export default combineReducers({
     auth,
     alert,
+    theme
 })
