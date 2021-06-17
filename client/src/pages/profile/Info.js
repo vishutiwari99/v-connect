dc6c1ff0e76fa5c6ec889559c1ed225d2418dc8e
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-17 13:28:56 +0530

    some UI enhancements

--- client/src/pages/profile/Info.js
+++ client/src/pages/profile/Info.js
@@ -11,16 +11,40 @@ const Info = () => {
         if (id === auth.username._id) {
             setUserData([auth.username]);
         }
-    }, [id, auth.user])
+    }, [id, auth.username])
     console.log(userData)
 
     return (
-        <div>
+        <div className="flex justify-center ">
             {
                 userData.map(user => (
-                    <div key={user._id}>
+                    <div className="flex flex-col items-center justify-between lg:flex-row" key={user._id}>
                         <Avatar src={user.avatar} />
+                        <div>
+                            <div className="flex justify-between items-center w-52">
+                                <h2 className="text-2xl">{user.username}</h2>
+                                <button className="transition duration-500 border-2 border-blue-400  bg-white text-blue-400 hover:bg-blue-400 hover:text-white py-2 px-4 rounded">
+                                    Edit Profile
+                                </button>
+                            </div>
+                            <div>
+                                <span>
+                                    {user.followers.length} Followers
+                                </span>
+                                <span>
+                                    {user.following.length} Following
+                                </span>
+                            </div>
+                            <h6>{user.fullname}</h6>
+                            <p>{user.address}</p>
+                            <h6>{user.email}</h6>
+                            <a href={user.website} target="_blank" rel="noreferrer">
+                                {user.website}
+                            </a>
+                            <p>{user.story}</p>
+                        </div>
                     </div>
+
                 ))
             }
         </div>
