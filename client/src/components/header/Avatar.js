19d9682248e1665e09b2350825ebb5a4c35d8ddf
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-17 13:27:47 +0530

    Added size props

--- client/src/components/header/Avatar.js
+++ client/src/components/header/Avatar.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Avatar = ({ src }) => {
+const Avatar = ({ src, size }) => {
     return <div className="text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center ">
         <span className="mr-1">
             {/* <span className="material-icons">
                 {icon}
             </span> */}
-            <img src={src} alt="avatar" className="h-6" />
+            <img src={src} alt="avatar" className={size} />
 
         </span>
     </div>
