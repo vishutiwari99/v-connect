ad42623200a89e0546995d672a61ce435e336ed5
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-19 00:17:12 +0530

    Some bugs fixed

--- client/src/pages/profile/[id].js
+++ client/src/pages/profile/[id].js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Info from './Info'
 import Posts from './Posts'
+import Loading from '../../components/notify/Loading'
 
 const Profile = () => {
+    const { profile } = useSelector(state => state)
     return (
-        <div>
-            <Info />
+        <>{
+            profile.loading ? <Loading /> :
+                <Info />
+        }
             <Posts />
-        </div>
+        </>
     )
 }
 
