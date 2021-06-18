b2cb166764772b861b200346060ebb37a3594e54
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-19 00:17:48 +0530

    added getUserProfile

--- client/src/pages/profile/Info.js
+++ client/src/pages/profile/Info.js
@@ -2,42 +2,49 @@ import React, { useEffect, useState } from 'react'
 import Avatar from '../../components/header/Avatar';
 import { useParams } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
+import { getProfileUsers } from '../../redux/actions/profileAction';
+
 const Info = () => {
     const { id } = useParams();
-    const { auth } = useSelector(state => state);
+    const { auth, profile } = useSelector(state => state);
     const dispatch = useDispatch();
     const [userData, setUserData] = useState([]);
+
     useEffect(() => {
         if (id === auth.username._id) {
             setUserData([auth.username]);
+        } else {
+            dispatch(getProfileUsers({ users: profile.users, id, auth }))
+            const newData = profile.users.filter(user => user._id === id)
+            setUserData(newData)
         }
-    }, [id, auth.username])
-    console.log(userData)
+    }, [id, auth, dispatch, profile.users])
+    console.log("t=yeh", userData)
 
     return (
-        <div className="flex justify-center ">
+        <>
             {
                 userData.map(user => (
-                    <div className="flex flex-col items-center justify-between lg:flex-row" key={user._id}>
+                    <div className="flex flex-col items-center justify-between mx-auto max-w-md lg:flex-row" key={user._id}>
                         <Avatar src={user.avatar} />
                         <div>
-                            <div className="flex justify-between items-center w-52">
-                                <h2 className="text-2xl">{user.username}</h2>
+                            <div className="flex justify-between items-center w-56 lg:w-64">
+                                <h2 className="text-4xl font-bold">{user.username}</h2>
                                 <button className="transition duration-500 border-2 border-blue-400  bg-white text-blue-400 hover:bg-blue-400 hover:text-white py-2 px-4 rounded">
                                     Edit Profile
                                 </button>
                             </div>
-                            <div>
-                                <span>
+                            <div className="flex justify-between">
+                                <span className="text-blue-400">
                                     {user.followers.length} Followers
                                 </span>
-                                <span>
+                                <span className="text-blue-400">
                                     {user.following.length} Following
                                 </span>
                             </div>
-                            <h6>{user.fullname}</h6>
-                            <p>{user.address}</p>
-                            <h6>{user.email}</h6>
+                            <h6 className="font-semibold text-gray-600">{user.fullname}</h6>
+                            <p >{user.address}</p>
+                            <h6 className="font-medium text-gray-600">{user.email}</h6>
                             <a href={user.website} target="_blank" rel="noreferrer">
                                 {user.website}
                             </a>
@@ -47,7 +54,7 @@ const Info = () => {
 
                 ))
             }
-        </div>
+        </>
     )
 }
 
