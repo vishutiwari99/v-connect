d6f78eb51b018923126148c3b56eaafd050e986a
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-19 00:18:15 +0530

    Added getProfileUser action method

--- client/src/redux/actions/profileAction.js
+++ client/src/redux/actions/profileAction.js
@@ -1,3 +1,4 @@
+import { getDataAPI } from '../../utils/fetchData';
 import { GLOBALTYPES } from './globalTypes';
 
 export const PROFILE_TYPES = {
@@ -8,4 +9,19 @@ export const PROFILE_TYPES = {
     GET_ID: 'GET_PROFILE_ID',
     GET_POSTS: 'GET_PROFILE_POSTS',
     UPDATE_POST: 'UPDATE_PROFILE_POST'
+}
+
+export const getProfileUsers = ({ users, id, auth }) => async (dispatch) => {
+    if (users.every(user => user._id !== id)) {
+        try {
+            dispatch({ type: PROFILE_TYPES.LOADING, payload: true })
+            const res = await getDataAPI(`user/${id}`, auth.token);
+            dispatch({ type: PROFILE_TYPES.GET_USER, payload: res.data })
+            dispatch({ type: PROFILE_TYPES.LOADING, payload: false })
+
+        } catch (err) {
+            dispatch({ type: GLOBALTYPES.ALERT, payload: { error: err.response.data.msg } })
+
+        }
+    }
 }
