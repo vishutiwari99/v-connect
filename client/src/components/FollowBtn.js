5a2ac0b007bba025c88c869d12a4e1bbe7e93c88
Vaibhav Tiwari <vishutiwari99@gmail.com>
2021-06-24 10:43:34 +0530

    Follow Button added

---
+++ client/src/components/FollowBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FollowBtn = () => {
+    return (
+        <button className="transition duration-500 border-2 border-blue-400  bg-white text-blue-400 hover:bg-blue-400 hover:text-white py-2 px-4 rounded">
+            Follow
+        </button>
+    )
+}
+
+export default FollowBtn
